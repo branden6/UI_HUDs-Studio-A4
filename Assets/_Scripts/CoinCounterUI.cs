52b695843903cd55f1089ba1c00c122d8c133fdd
branden kennedy <brandenk88@gmail.com>
2025-03-10 11:37:37 -0700

    Created sliding animation for score
    implemented sliding animation for score!

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+using DG.Tweening;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+
+    private float containerInitPosition;
+    private float moveAmount;
+    void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    public void UpdateScore(int score){
+        toUpdate.SetText($"{score}");
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
+
+        StartCoroutine(ResetCoinContainer(score));
+    }
+    private IEnumerator ResetCoinContainer(int score){
+        yield return new WaitForSeconds(duration);
+
+        current.SetText($"{score}");
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
